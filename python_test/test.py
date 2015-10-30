31a19fc8f074a8c94413eddeab7d60dcca8f3fe4
karunru <1qazhikaru@gmail.com>
2015-10-30 16:34:43 +0900

    完成したっぽい

--- python_test/test.py
+++ python_test/test.py
@@ -1,25 +1,29 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
+# 外部データの読み込み
 data = np.loadtxt("test_data",delimiter=",")
-print(data)
-rx = data[:,0]
+
+# 実データをrベクトルに
+rx = data[:,0] # 列の取り出し
 ry = data[:,1]
 
+# 平均を計算
 averx = np.average(rx)
 avery = np.average(ry)
 
+# rベクトルをxベクトルに変換(原点を中心にする)
 xx = rx - averx
 xy = ry - avery
 
+# 分散共分散行列を計算
 V = np.zeros((2,2))
-
-V[0][0] = np.dot(rx-averx,rx-averx.T) / 30
+V[0][0] = np.dot(rx-averx,rx-averx.T) / 30 # ベクトルと転置したベクトルの内積でシグマ計算を実現
 V[0][1] = np.dot(rx-averx,ry-avery.T) / 30
 V[1][0] = np.dot(ry-avery,rx-averx.T) / 30
 V[1][1] = np.dot(ry-avery,ry-avery.T) / 30
 
+# 分散共分散行列の固有値,固有ベクトルを計算
 la, u = np.linalg.eig(V)
 
 print(averx)
@@ -27,13 +31,27 @@ print(avery)
 print(V)
 print(la)
 print(u)
-print(u[:,0])
 
-#new_x = u.T*V*u*rx - averx
-#new_y = u.T*V*u*ry - avery
+# グラフに直線を書く準備
+u1x = np.arange(-30,30,1) # -30から30まで1刻みのデータを作成
+u1y = np.arange(-30,30,1)
+u2x = np.arange(-30,30,1)
+u2y = np.arange(-30,30,1)
 
-# plt.plot(rx,ry,"bo")
-#plt.plot([0,0],u[:,0], "-k")
-plt.plot(xx,xy,"ro")
-#plt.plot(new_x,new_y,"ro")
+# 固有ベクトル倍
+u1x = u1x * u[0][0]
+u1y = u1y * u[1][0]
+u2x = u2x * u[0][1]
+u2y = u2y * u[1][1]
+
+# u1とu2が直交してないっぽく見えたから内積0になるかどうかで確認
+u1 = u[:,0]
+u2 = u[:,1]
+
+print(np.dot(u1,u2))
+
+# plt.plot(rx,ry,"bo") # 実データを青でプロット
+plt.plot(u1x,u1y,"-r") # u1を赤でプロット
+plt.plot(u2x,u2y,"-r") # u2を赤でプロット
+plt.plot(xx,xy,"go") # 原点移動したデータを緑でプロット
 plt.show()
