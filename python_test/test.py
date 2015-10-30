1b2fdaec8995a28a9e6e68bd8f7e7b848348e688
karunru <1qazhikaru@gmail.com>
2015-10-30 10:37:56 +0900

    平均を座標軸にした

--- python_test/test.py
+++ python_test/test.py
@@ -2,18 +2,38 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("test_data.csv",header=None)
+data = np.loadtxt("test_data",delimiter=",")
+print(data)
+rx = data[:,0]
+ry = data[:,1]
 
-print(df)
+averx = np.average(rx)
+avery = np.average(ry)
 
-height = df[0]
-weight = df[1]
+xx = rx - averx
+xy = ry - avery
 
-plt.plot(height,weight,"o")
+V = np.zeros((2,2))
+
+V[0][0] = np.dot(rx-averx,rx-averx.T) / 30
+V[0][1] = np.dot(rx-averx,ry-avery.T) / 30
+V[1][0] = np.dot(ry-avery,rx-averx.T) / 30
+V[1][1] = np.dot(ry-avery,ry-avery.T) / 30
+
+la, u = np.linalg.eig(V)
+
+print(averx)
+print(avery)
+print(V)
+print(la)
+print(u)
+print(u[:,0])
+
+#new_x = u.T*V*u*rx - averx
+#new_y = u.T*V*u*ry - avery
+
+# plt.plot(rx,ry,"bo")
+#plt.plot([0,0],u[:,0], "-k")
+plt.plot(xx,xy,"ro")
+#plt.plot(new_x,new_y,"ro")
 plt.show()
-
-avex = np.average(height)
-avey = np.average(weight)
-
-print(avex)
-print(avey)
